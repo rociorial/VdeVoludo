dc83416feaa419d62937729399119e1c93c7b87e
rociorial <147448807+rociorial@users.noreply.github.com>
2024-09-19 01:55:57 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+<!-- Ocultar resto de menú con JavaScript -->
+    <script>
+      document.addEventListener('DOMContentLoaded', () => {
+            const links = document.querySelectorAll('a[data-target]');
+
+            links.forEach(link => {
+                link.addEventListener('click', (event) => {
+                    event.preventDefault();
+                    const targetId = link.getAttribute('data-target');
+                    document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+                });
+            });
+        });
+    </script>
+</body>
+</html>
 
