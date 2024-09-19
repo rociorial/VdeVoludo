3a7a388a187f3d60e3b1e050c47a0e3481f577f6
rociorial <147448807+rociorial@users.noreply.github.com>
2024-09-19 22:50:23 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,31 @@
 <!-- Ocultar resto de menú con JavaScript -->
-    <script>
-      document.addEventListener('DOMContentLoaded', () => {
-            const links = document.querySelectorAll('a[data-target]');
+<script>
+  document.addEventListener('DOMContentLoaded', () => {
+    const links = document.querySelectorAll('a[data-target]');
 
-            links.forEach(link => {
-                link.addEventListener('click', (event) => {
-                    event.preventDefault();
-                    const targetId = link.getAttribute('data-target');
-                    document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
-                });
-            });
-        });
-    </script>
-</body>
-</html>
+    links.forEach(link => {
+      link.addEventListener('click', (event) => {
+        event.preventDefault();
+        const targetId = link.getAttribute('data-target');
+        document.getElementById(targetId).scrollIntoView({ behavior: 'smooth' });
+      });
+    });
+  });
+</script>
+        
+<!-- Navegador móvil -->
+<script>
+  document.addEventListener('DOMContentLoaded', function() {
+    // Selecciona el ícono de menú y el menú de navegación
+    const menuToggle = document.querySelector('.menu-toggle');
+    const navbarMenu = document.querySelector('.navbar ul');
+
+    // Añade un evento de clic al ícono de menú
+    menuToggle.addEventListener('click', function() {
+      // Alterna la clase 'show' en el menú de navegación
+      navbarMenu.classList.toggle('show');
+    });
+  });
+</script>
+
 
